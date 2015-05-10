efb7b176d5b14dbe296d91cdc9cdd7b1c138e639
Isaac González <vyrkan@me.com>
2015-05-10 09:45:21 -0500

    new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:account_update) << :name ## add the attributes you want to permit
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:account_update) << :name
   end
 end
