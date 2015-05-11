305dac41084b981363b137e5e3b2ad1932508c92
Isaac González <vyrkan@me.com>
2015-05-10 21:26:27 -0500

    er

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,7 @@
 class Pin < ActiveRecord::Base
 	acts_as_votable
 	belongs_to :user
-has_attached_file :image, :styles => { :medium => "300x300>" }
+has_attached_file :image, :styles => { large: "600x600>", medium: "300x300>", thumb: "150x150#" }
 validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 validates_attachment_presence :image
 end
