53f8829803cd6447e950869c9d6823188a7dbaa1
Isaac González <vyrkan@me.com>
2015-05-10 20:57:26 -0500

    new masonry

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,12 +14,5 @@
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require masonry/jquery.masonry
-//= require jquery.turbolinks
-//= require jquery_ujs
-//= require bootstrap
-//= require masonry/jquery.imagesloaded.min
-//= require masonry/jquery.infinitescroll.min
-//= require masonry/modernizr-transitions
-//= require underscore
-//= require gmaps/google
+//= require turbolinks
 //= require_tree .
