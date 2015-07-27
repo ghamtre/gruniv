d9f93a8ac74cc0d0c7bd3e4ce6dc58a4af0d65a2
Isaac González <vyrkan@me.com>
2015-07-27 08:35:56 -0500

    new page

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,3 @@
 Rails.application.routes.draw do
-  get '/links/subregion_options' => 'links#subregion_options'
-  resources :comments
-  resources :pins
-  resources :links 
-  devise_for :users
-  resources :links do
-    member do
-      put "like", to: "links#upvote"
-      put "dislike", to: "links#downvote"
-    end
-    resources :comments
-  end
-  resources :pins do 
-    member do 
-      put "like", to: "pins#upvote"
-    end
-  end
-  
-  authenticated :user do
-  root 'links#index', as: "authenticated_root"
-end
-  root 'index'
+  root 'pages#home'
 end
